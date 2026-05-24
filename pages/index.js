b560069bf7baad2147c97df3ac964a20eb688a4b
dantec0de <devdante2008@gmail.com>
2026-05-24 16:24:16 +0000

    deixando a página com  status "Em construção"

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 function Home(){
-    return <h1>Em construção...
-        Acompanhe para ver o progresso
-    </h1>
+    return <div>
+        <h1>Em construção...</h1>
+        <p>Acompanhe para ver o progresso</p>
+           </div>
 }
 
 export default Home
