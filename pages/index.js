c5b3d10498100e17359535d69506977ea3871917
dantec0de <devdante2008@gmail.com>
2026-05-24 16:49:38 +0000

    deixando a pagina com Status de 'Em construção'

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ function Home(){
     return <div>
         <h1>Em construção...</h1>
         <p>Acompanhe para ver o progresso</p>
+        <p>by: André Santos(dantec0de)</p>
            </div>
 }
 
